18c6c994969c0127d30571a0aff390b0ed60aae4
Luiz Pires <lpiresjunior@gmail.com>
2021-01-03 12:14:39 -0300

    select episode

--- src/pages/Podcast/index.js
+++ src/pages/Podcast/index.js
@@ -28,11 +28,11 @@ class Podcast extends Component {
     navigation.navigate('Main');
   };
 
-  handlePlay = () => {
+  handlePlay = (episodeId) => {
     const {setPodcastRequest, navigation} = this.props;
     const podcast = navigation.getParam('podcast');
 
-    setPodcastRequest(podcast);
+    setPodcastRequest(podcast, episodeId);
   };
 
   render() {
@@ -62,7 +62,7 @@ class Podcast extends Component {
           data={podcast.tracks}
           keyExtractor={(episode) => String(episode.id)}
           renderItem={({item: episode}) => (
-            <Episode>
+            <Episode onPress={() => this.handlePlay(episode.id)}>
               <Title>{episode.title}</Title>
               <Author>{episode.artist}</Author>
             </Episode>
